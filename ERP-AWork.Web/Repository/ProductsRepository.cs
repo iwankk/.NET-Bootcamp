cf80e0fe397a06eb06bb482a22ac01888486165a
iwankk <iwankartiwan77@gmail.com>
2022-09-15 17:47:38 +0700

    Done

--- ERP-AWork.Web/Repository/ProductsRepository.cs
+++ ERP-AWork.Web/Repository/ProductsRepository.cs
@@ -11,13 +11,13 @@ namespace ERP_AWork.Web.Repository
             new Products { ProductId = 1, ProductName = "Baud", ProductCategory = "Sparepart" },
             new Products { ProductId = 2, ProductName = "Roller AS", ProductCategory = "Sparepart" },
             new Products { ProductId = 3, ProductName = "ShockBreaker", ProductCategory = "Sparepart" },
-<<<<<<< HEAD
+//<<<<<<< HEAD
             new Products { ProductId = 6, ProductName = "Oli", ProductCategory = "Sparepart" },
-            new Products { ProductId = 6, ProductName = "OliSamping", ProductCategory = "Sparepart" }
+            new Products { ProductId = 6, ProductName = "OliSamping", ProductCategory = "Sparepart" },
 
-=======
+//=======
             new Products { ProductId = 5, ProductName = "gerandong", ProductCategory = "bolokokok" }
->>>>>>> Coba2
+//>>>>>>> Coba2
 
         };
             return ListOfProducts;
